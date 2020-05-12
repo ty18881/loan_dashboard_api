b0c7ae531274683565b074ed71db639ecb9e0711
ty18881 <ty18881@gmail.com>
2020-05-11 20:58:28 -0400

    Application Controller - adjust parameter reference - now can create underlying loan when the application is created via Postman

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -22,38 +22,54 @@ class ApplicationsController < ApplicationController
     # @application = Application.new(application_params)
 
     puts 'Examine the input parameters'
-    puts params.inspect
+    puts application_params.inspect
     puts 'Instantiating the new Application record'
     @application = Application.new
     @application.submit_date = Date.current
     puts "date setting completed #{@application.submit_date}"
     @application.status = 'pending'
     puts "status set: #{@application.status}"
-    @application.borrower_id = params[:borrower]
-    puts "borrower specified #{params[:borrower_id]}"
+    
+    @application.borrower_id = params[:application][:borrower_id]
+    
     puts 'Instantiation completed'
-    # puts 'grabbing borrower id from the params'
-    # puts "Borrower Id: #{params[:borrower_id]}"
-    # puts "Principal Amount: #{params[:principal_amount]}"
-
-    # @application.borrower_id = params[:borrower_id]
+    
     
 
     if @application.save
       puts 'DB Save success'
-      render json: @application, status: :created, location: @application
+      
       # now it is safe to instantiate and store the new underlying loan information.
       # puts 'Application POST route - creating underlying loan record'
-      # @loan = Loan.new
-      # @loan.principal_amount = params[:principal_amount]
-      # @loan.interest_rate = params[:interest_rate]
-      # @loan.maturity_date = params[:maturity_date]
-      # @loan.term = params[:term]
-      # @loan.application_id = @application.id
+      @loan = Loan.new
+      @loan.principal_amount = params[:application][:principal_amount]
+      @loan.interest_rate = params[:application][:interest_rate]
+      @loan.maturity_date = params[:application][:maturity_date]
+      @loan.term = params[:application][:term]
+      @loan.application_id = @application.id
 
-      # puts 'Application POST route - saving underlying loan record'
+      puts 'Application POST route - saving underlying loan record'
 
-      # @loan.save
+      @loan.save
+puts 'created the underlying loan'
+      ## now update the appsubmits table with the new record
+      ## first, were there other applications made on your same date?
+      ## if so, increment the existing counter
+      ## otherwise, create a new record.
+
+      @appsubmit = Appsubmit.find_by submit_date: Date.current
+
+      if @appsubmit 
+        ## we found a record with the current date.  Increment the counter
+        @appsubmit.count = @appsubmit.count + 1
+        @appsubmit.save
+      else
+        @appsubmit= Appsubmit.new(submit_date: Date.current, count: 1)
+        @appsubmit.save
+      end
+
+      puts 'Updated the join table so the graph will get the new data'
+      render json: @application, status: :created, location: @application
     else
       render json: @application.errors, status: :unprocessable_entity
     end
@@ -92,6 +108,25 @@ class ApplicationsController < ApplicationController
     ## .permit => specifies the list of attributes as individual symbols to ensure only those can be passed to Application.create
     ## params.require(:application).permit! => any attributes nested under the :application key to be set on the Application active record.
     def application_params
-      params.require(:application).permit(:borrower)
+      params.require(:application).permit(:submit_date, :decision_date, :review_date, :status, :borrower_id, :principal_amount, :interest_rate, :maturity_date, :term)
     end
 end
+
+# create_table "loans", force: :cascade do |t|
+#   t.decimal "principal_amount"
+#   t.decimal "interest_rate"
+#   t.date "maturity_date"
+#   t.integer "term"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.integer "application_id"
+
+
+# create_table "applications", force: :cascade do |t|
+#   t.date "submit_date"
+#   t.date "decision_date"
+#   t.date "review_date"
+#   t.string "status"
+#   t.datetime "created_at", precision: 6, null: false
+#   t.datetime "updated_at", precision: 6, null: false
+#   t.integer "borrower_id"
